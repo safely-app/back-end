ac6f87c44c6353bd308410b2df1bac04627b8b5f
Louis Christner <louis.christner@epitech.eu>
2022-11-19 14:34:31 +0100

    Fix url typo

--- commercial/store/utils.js
+++ commercial/store/utils.js
@@ -3,7 +3,7 @@ import fetch from 'node-fetch';
 export async function getSafeplace(safeplaceId, authorization) {
   try {
     const options = { method: 'GET', headers: { 'Authorization': authorization } };
-    const safeplaceResponse = await fetch(`https://api.safely-app.fr/safeplace/${safeplaceId}`, options);
+    const safeplaceResponse = await fetch(`https://api.safely-app.fr/safeplace/safeplace${safeplaceId}`, options);
 
     return await safeplaceResponse.json();
   } catch (err) {
